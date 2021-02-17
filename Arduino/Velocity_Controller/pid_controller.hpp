00f9cc7de48bc188dfb9f424a5d7f38b27e89cef
dacsgb <43976345+dacsgb@users.noreply.github.com>
2021-02-16 18:36:07 -0600

    Changed some variable names and compute equations

--- Arduino/Velocity_Controller/pid_controller.hpp
+++ Arduino/Velocity_Controller/pid_controller.hpp
@@ -10,60 +10,68 @@ Purpose:    Compute control variable using PID given setpoint and measured value
 class PIDController {
     private:
     
-    float kp, ki, kd;
-    float integral = 0.0;
-    float derivative;
-    float previousError = 0.0;
-    float error;
+        float kp, ki, kd;
+        float integral = 0.0;
+        float error [2] = {0.0,0.0};
+        unsigned long time[2] = {0.0,0.0};
 
-    int upperControlLimit = 255;
-    int lowerControlLimit = 0;
+        float umax = 1;
+        float umin = 0;
 
     public:
 
-    PIDController(float KP, float KI, float KD) {
-        kp = KP;
-        ki = KI;
-        kd = KD;
-    }
+        PIDController(float KP, float KI, float KD) {
+            kp = KP;
+            ki = KI;
+            kd = KD;
+        }
 
-    void setControlLimits(int upperLimit, int lowerLimit) {
-        upperControlLimit = upperLimit;
-        lowerControlLimit = lowerLimit;
-    }
+        void setControlLimits(int upperLimit, int lowerLimit) {
+            umin = upperLimit;
+            umax = lowerLimit;
+        }
 
-    /**Compute and return control value from PID.
-     * @param float Desired value to reach; setpoint
-     * @param float Measured value; actual
-     * @param unsigned long milliseconds passed since last measure
-     * @return float Control value
-     */
-    float computeControl(float setpoint, float measured, unsigned long timePassed) {
+        /**Compute and return control value from PID.
+         * @param float Desired value to reach; setpoint
+         * @param float Measured value; actual
+         * @param unsigned long milliseconds passed since last measure
+         * @return float Control value
+         */
+        float computeControl(float setpoint, float measured, unsigned long t1) {
+            
+            // Store current error and time into arrays
+            error[1] = setpoint - measured;
+            time[1] = t1;
+
+            // Calculate derivative and integral
+            float derivative = (error[1]-error[0]) / (time[1]-time[0]);
+            float dintegral = (error[1]-error[0]) * (time[1]-time[0]);
+            integral += dintegral;
+
+            // Calculate control input
+            float control = (kp*error[1]) + ki*integral + (kd*derivative);
+
+            // Intrgeal windup correction
+            if((control > umax) || (control < umin)){
+                control -= ki*integral;
+                integral -= dintegral;
+            } 
         
-        error = setpoint - measured;
-        integral += error * timePassed * ki;
-        derivative = (error - previousError) / timePassed;
+            //Bound control term within correct range
+            control = max(umin,min(umax,control));
 
-        //Stop integral windup
-        if(integral > upperControlLimit) integral = upperControlLimit;
-        else if(integral < lowerControlLimit) integral = lowerControlLimit;
-        
-        float control = (error * kp) + integral + (derivative * kd);
+            // Shift values in error and time array
+            error[0] = error[1];
+            time[0] = time[1];
 
-        //Bound control term within correct range
-        if(control > upperControlLimit) control = upperControlLimit;
-        else if(control < lowerControlLimit) control = lowerControlLimit;
+            return control;
 
-        previousError = error;
-
-        return control;
-
-        //Room for improvement:
-        //derivative kick,
-        //PID handles frequency
-        //Changing frequency on the fly
-        //Changing tunings on the fly (works better with preset period)
-   }
+            //Room for improvement:
+            //derivative kick,
+            //PID handles frequency
+            //Changing frequency on the fly
+            //Changing tunings on the fly (works better with preset period)
+    }
 };
 
 #endif //PID_CONTROLLER_HPP
