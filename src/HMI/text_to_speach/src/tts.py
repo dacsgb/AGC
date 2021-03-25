5d23d75f4c596226a775ad59949105cf2dd2ed5e
dacsgb <dcolon@okstate.edu>
2021-03-25 15:34:39 -0500

    tts node done

--- src/HMI/text_to_speach/src/tts.py
+++ src/HMI/text_to_speach/src/tts.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
 
-
 import pyttsx3
-import time
+import rospy
 
+from std_msgs.msg import String
 
-engine = pyttsx3.init()
-engine.setProperty('rate',250)
-engine.say("FUCK")
-engine.runAndWait() 
+class TTS():
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate',175)
+
+        self.text_sub = rospy.Subscriber("/text",String,self.text_cb)
+        self.rate = rospy.Rate(5)
+
+    def text_cb(self,msg):
+        self.say(msg.data)
+
+    def say(self,text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+    
+    def run(self):
+        self.rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node("tts")
+
+    Node = TTS()
+    Node.say("All systems are operational")
