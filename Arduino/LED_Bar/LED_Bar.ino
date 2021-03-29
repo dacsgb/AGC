23fbad0a80fd0b9d7d2741f23e66bdf6fd09529d
Collin Thornton <collin.thornton@okstate.edu>
2021-03-29 12:18:56 -0500

    Finished LED bar

--- Arduino/LED_Bar/LED_Bar.ino
+++ Arduino/LED_Bar/LED_Bar.ino
@@ -3,38 +3,58 @@
 #include <TimerOne.h>  
 #include "SystemFont5x7.h"
 #include "Arial_black_16.h"
+#include <ros.h>
+#include <std_msgs/String.h>
 
 #define DISPLAYS_ACROSS 3
 #define DISPLAYS_DOWN 1
 DMD dmd(DISPLAYS_ACROSS, DISPLAYS_DOWN);
 
-char* texto = "GOD HELP US";
+char* texto = "Boot Up";
+
+//draw the text
+void messagecb(const std_msgs::String& str) {
+  dmd.clearScreen(true);
+  delay(10);
+  drawText(str.data);
+  Serial.println(str.data);
+  delay(10);
+}
+
+
+//Ros
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::String> sub("HMI/led", messagecb);
 
 void ScanDMD(){dmd.scanDisplayBySPI();}
 
 void setup(void){ 
-  Serial.begin(115200);
+  Serial.begin(57600);
   
   Timer1.initialize(1000);
   Timer1.attachInterrupt(ScanDMD);
 
   //clear/init the DMD pixels held in RAM
   dmd.selectFont(SystemFont5x7);
-  dmd.clearScreen(false);
-  delay(3000);
   dmd.clearScreen(true);
+
+  nh.initNode();
+  nh.subscribe(sub);
+
+  drawText(texto);
 }
 
 
 void loop() {
-  //drawText(texto);
+  nh.spinOnce();
 }
 
 void drawText(char* text){
   int len = String(text).length();
-  dmd.drawString( int((32*DISPLAYS_ACROSS - 6*len)/2),4, texto, len, GRAPHICS_NORMAL );
+  dmd.drawString( int((32*DISPLAYS_ACROSS - 6*len)/2),4, text, len, GRAPHICS_NORMAL );
 }
 
+/*
 void Right()
 {
   int i=8;
@@ -77,3 +97,4 @@ dmd.drawString( x,4, "Stopping", 8, GRAPHICS_NORMAL );
 dmd.drawFilledBox(0,0,20,16,GRAPHICS_NORMAL);
 dmd.drawFilledBox(76,0,96,16,GRAPHICS_NORMAL);
 }
+*/
