13ee087e1fce5ee96fcbc738a1596b63bbab4da7
Collin Thornton <collin.thornton@okstate.edu>
2021-04-12 13:00:53 -0500

    updated usb camera node

--- src/Sensors/usb_cam_driver/src/usb_camera.py
+++ src/Sensors/usb_cam_driver/src/usb_camera.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import numpy as np
 
 import rospy
@@ -6,42 +6,34 @@ import cv2
 from sensor_msgs.msg import Image, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
+
 class Node():
-    def __init__(self,width,height,fps,cam):
+    def __init__(self,topic_name):
         
-        self.image_pub = rospy.Publisher("/camera/original",Image,queue_size=1)
-        self.compressed_pub = rospy.Publisher("/camera/compressed",CompressedImage,queue_size=1)
+        self.image_pub = rospy.Publisher(topic_name+"/original",Image,queue_size=1)
+        self.compressed_pub = rospy.Publisher(topic_name+"/compressed",CompressedImage,queue_size=1)
         self.comp_img = CompressedImage()
 
-        self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-        
-        self.bridge = CvBridge()
-
-        self.cap = cv2.VideoCapture(cam)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, float(width))
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, float(height))
-
-        self.rate = rospy.Rate(fps)
-
-    def run(self):
+    def run(self,src):
+        rate = rospy.Rate(src.get(cv2.CAP_PROP_FPS))
+        bridge = CvBridge()
         while not rospy.is_shutdown():
-            _, frame = self.cap.read()
-
-            #self.image_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
-
-            self.comp_img.header.stamp = rospy.Time.now()
-            self.comp_img.format = "jpeg"
-            self.comp_img.data = np.array(cv2.imencode('.jpg', frame)[1]).tostring()
-            self.compressed_pub.publish(self.comp_img)
-            self.rate.sleep()
+            success, frame = src.read()   
+            if success:
+                self.image_pub.publish(bridge.cv2_to_imgmsg(frame, encoding="bgr8"))
+                self.compressed_pub.publish(bridge.cv2_to_compressed_imgmsg(frame))
+            rate.sleep()
+        src.release()
     
 if __name__ == "__main__":
     rospy.init_node("usb_camera")
 
-    height = rospy.get_param('~height')
-    width = rospy.get_param('~width')
-    fps = rospy.get_param('~fps')
-    cam = rospy.get_param('~camera_num')
+    camera = cv2.VideoCapture(rospy.get_param('~camera_num', 0))
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, rospy.get_param('~width', camera.get(cv2.CAP_PROP_FRAME_WIDTH)))
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, rospy.get_param('~height', camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    camera.set(cv2.CAP_PROP_FPS, rospy.get_param('~fps',30))
+    
+    topic_name = rospy.get_param("~topic_name","/usb_camera")
 
-    Camera = Node(width,height,fps,cam)
-    Camera.run()
+    Camera = Node(topic_name)
+    Camera.run(camera)
