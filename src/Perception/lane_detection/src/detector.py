997798aba093967ddf9f70818716e39f46655f97
dacsgb <dcolon@okstate.edu>
2021-04-16 08:37:56 -0500

    Claculated positioning error

--- src/Perception/lane_detection/src/detector.py
+++ src/Perception/lane_detection/src/detector.py
@@ -18,18 +18,10 @@ class Detector():
 
         self.img_sub = rospy.Subscriber("/{}/image_color".format(camera),Image,self.imgCB)
         self.cam_sub = rospy.Subscriber("/{}/camera_info".format(camera),CameraInfo,self.camCB)
-        self.loaded = False
-
-        self.fits = []
+        self.img = None
 
         self.colorMasks = np.array([[[0,0,200],[255,55,255]],
                                     [[22,93,0],[45,255,255]]])
-
-        self.mapping = np.float32([[[175, 120],[465, 120],[640,280],[0, 280]],
-                                    [[0, 0],[640, 0],[640, 280],[0, 280]]])
-
-
-        self.M = cv2.getPerspectiveTransform(self.mapping[0], self.mapping[1])
 
     def camCB(self,msg):
         self.dist = np.array(msg.D)
@@ -38,27 +30,20 @@ class Detector():
 
     def imgCB(self,msg):
         bridge = CvBridge()
-        img = bridge.imgmsg_to_cv2(msg,desired_encoding="bgr8")
-        undist = self.undistort(img)
-        bev = self.BEV(undist)
-        markers = self.markers(bev,self.colorMasks)
-        self.fits = self.fit(markers)
-        predict = self.draw_lanes(markers,self.fits)
-        self.display(undist,bev,predict)
-        
+        self.img = bridge.imgmsg_to_cv2(msg,desired_encoding="bgr8")
 
     def undistort(self,img):
         return cv2.undistort(img, self.mtx, self.dist, None, self.camMtx)[self.roi[1]:self.roi[1]+self.roi[3], self.roi[0]:self.roi[0]+self.roi[2]]
 
     def BEV(self,img):
-        return cv2.warpPerspective(img, self.M, (img.shape[1],img.shape[0]))
+        mapping = np.float32([[[18.34, 493.96],[163.41,426.91],[866.35,426.93],[965.41,493.32]],
+                                [[0, img.shape[0]],[0, 0],[img.shape[1],0],[img.shape[1],img.shape[0]]]])
+        M = cv2.getPerspectiveTransform(mapping[0], mapping[1])
+        return cv2.warpPerspective(img, M, (img.shape[1],img.shape[0]))
 
     def markers(self,img,masks):
-        processed = []
         hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-        for mask in masks:
-            masked = cv2.bitwise_and(img,img,mask=cv2.inRange(hsv,np.array(mask[0]),np.array(mask[1])))
-            processed.append(masked)
+        processed = [cv2.bitwise_and(img,img,mask=cv2.inRange(hsv,np.array(mask[0]),np.array(mask[1]))) for mask in masks]
         return processed
 
     def fit(self,imgs):
@@ -66,12 +51,17 @@ class Detector():
         for img in imgs:
             points = np.argwhere(img > 120)
             try:
-                fits.append(np.polyfit(points[:,0],points[:,1],2))
+                fits.append(np.poly1d(np.polyfit(points[:,0],points[:,1],2)))
             except:
                 fits.append([])
-
         return fits
-        
+
+    def est(self,img,fit):
+        h, w, c = img.shape
+        est = [f(h) for f in fit]
+        avg = np.average(est)
+        return avg, w/2 - avg
+
     def draw_lanes(self,imgs,fits):
         for i, img in enumerate(imgs):
             x = np.arange(0,img.shape[0])
@@ -81,18 +71,32 @@ class Detector():
                 img = cv2.circle(img,(int(y[i]),int(x[i])),radius=0,color=(255,0,0),thickness=-1)
         return imgs
 
-    def display(self,original,bev,imgs):
+    def display(self,original,bev,imgs,est):
+        res = imgs[0]
         for i, img in enumerate(imgs):
-            cv2.imshow("Prediction: {}".format(i),img)
+            res = cv2.bitwise_or(res,img)
+        cv2.circle(res,(int(est),res.shape[0]),3,(0,0,255),-1)
+        cv2.line(res,(int(res.shape[1]/2),0),(int(res.shape[1]/2),res.shape[0]),(0,255,0),2 )
         cv2.imshow("Undistorted",original)
         cv2.imshow("BEV",bev)
+        cv2.imshow("Masks",res)
         cv2.waitKey(3)
 
     def run(self): 
         rate = rospy.Rate(60)
         while not rospy.is_shutdown():
-            print(self.fits)
-            
+            if self.img is not None:
+                undist = self.undistort(self.img)
+                bev = self.BEV(undist)
+                markers = self.markers(bev,self.colorMasks)
+                fits = self.fit(markers)
+                estimate, err = self.est(bev,fits)
+                
+                print("Positioning Error = {:.3f} [m]".format(3.048*err/1003))
+                
+                predict = self.draw_lanes(markers,fits)
+                self.display(undist,bev,predict,estimate)
+
             rate.sleep()
 
 
